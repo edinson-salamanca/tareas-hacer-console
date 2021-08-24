e60406883375ef38e4269ab6cde1ba8bd75cf432
Edinson Salamanca <sennin7k@gmail.com>
2021-08-23 21:35:30 -0500

    menu created with interactive options

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const { inquirerMenu } = require('./helpers/inquirer');
+
 const main = async () => {
-    
+    let opt = await inquirerMenu();
+    console.log(opt);
 };
 
 main();
---
+++ helpers/inquirer.js
@@ -0,0 +1,65 @@
+const inquirer = require('inquirer');
+const Choices = require('inquirer/lib/objects/choices');
+
+require('colors');
+
+const choices = new Choices([
+    {
+        value: 1,
+        name: `${'1'.green} Crear tarea`,
+    },
+    {
+        value: 2,
+        name: `${'2'.green} Listar tareas`,
+    },
+    {
+        value: 3,
+        name: `${'3'.green} Listar tareas completadas`,
+    },
+    {
+        value: 4,
+        name: `${'4'.green} Listar tareas pendientes`,
+    },
+    {
+        value: 5,
+        name: `${'5'.green} Completar tarea(s)`,
+    },
+    {
+        value: 6,
+        name: `${'6'.green} Borrar tarea`,
+    },
+    {
+        value: 0,
+        name: `${'0'.green} Salir`,
+    },
+]);
+
+const questions = [
+    {
+        type: 'list',
+        name: 'option',
+        message: '¿Que deseas hacer?',
+        choices,
+    },
+];
+
+const inquirerMenu = async () => {
+    console.clear()
+    showHeader();
+    const { option } = await inquirer.prompt(questions);
+
+    return option;
+};
+
+const showHeader = () => {
+    let message = `
+        ${'=========================='.green}
+           ${'Seleccione una opción'}
+        ${'=========================='.green}
+    `;
+
+    console.log(message);
+};
+module.exports = {
+    inquirerMenu,
+};
