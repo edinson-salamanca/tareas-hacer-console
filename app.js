40663d348e9be9cbff7ac0b6e267b262bb38b756
Edinson Salamanca <sennin7k@gmail.com>
2021-10-27 17:19:16 -0500

    create tasks implemented correctly

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const { inquirerMenu, pause } = require('./helpers/inquirer');
+const { inquirerMenu, pause, readInput } = require('./helpers/inquirer');
+const Tasks = require('./models/tasks');
 
 const main = async () => {
+    const tasks = new Tasks();
     const opt = await inquirerMenu();
 
     if (opt !== 0) {
+        switch (opt) {
+            case 1: {
+                const desc = await readInput('Descripción');
+                tasks.createTask(desc);
+                break;
+            }
+        }
         await pause();
         await main();
     }
